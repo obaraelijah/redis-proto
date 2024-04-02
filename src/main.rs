9e49f75bbf39e779ccdf133ac504899d19592ed1
obaraelijah <elijahobara357@gmail.com>
2024-04-02 20:51:39 +0300

    feat: Implement basic functionality in the main function to initialize the state, load the database state, and start the server with periodic saving.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,32 @@
 use redis_proto::logger::LOGGER;
 use redis_proto::startup::{startup_message, Config};
+use redis_proto::database::{get_dump_file, load_state, save_state_interval};
 
-use slog::info;
+use slog::{info, warn};
 use structopt::StructOpt;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Get the args
     let opt = Config::from_args();
     // print the fancy logo
     startup_message(&opt);
-    // database state
+    // Get the database file, making folders if necessary.
     info!(LOGGER, "Initializing state...");
-    todo!()
+    let dump_file = get_dump_file(&opt);
+    //  Load database state if it exists.
+    info!(LOGGER, "Opening Datafile...");
+    let state = load_state(dump_file.clone(), &opt)?;
+    // Spawn the save occassionally service
+    info!(LOGGER, "Starting Server...");
+    if !opt.memory_only {
+        info!(LOGGER, "Spawning database saving task...");
+        tokio::spawn(save_state_interval(state.clone(), dump_file.clone()));
+    } else {
+        warn!(
+            LOGGER,
+            "Database is in memory-only mode. STATE WILL NOT BE SAVED!"
+        );
+    }
+    Ok(())
 }
