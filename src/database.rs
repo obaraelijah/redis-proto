176737c66ff970437482e3125e90a7705ceb388b
obaraelijah <elijahobara357@gmail.com>
2024-04-02 18:07:19 +0300

    feat: Serialize and write current state to dump file and configure opts

--- src/database.rs
+++ src/database.rs
@@ -1,1 +1,44 @@
+use crate::logger::LOGGER;
+use crate::startup::Config;
+use crate::types::{Dumpfile, StateRef, StateStoreRef};
+use directories::ProjectDirs;
+use parking_lot::Mutex;
+use std::error::Error;
+use std::fs::{File, OpenOptions};
+use std::io::{Seek,SeekFrom};
+use std::path::{Path, PathBuf};
+use std::sync::Arc;
+use std::time::Duration;
+use tokio::{task, time::Interval};
+use rmp_serde as rmps;
+use slog::error;
 
+
+/// Convenience macro to panic with error messages.
+macro_rules! fatal_panic {
+    ($msg:expr) => {{
+        error!(LOGGER, "{}", $msg);
+        println!("{}", $msg);
+        panic!("Fatal Error, cannot continue...");
+    }};
+    ($msg:expr, $err:expr) => {{
+        error!(LOGGER, "{} {}", $msg, $err);
+        println!("{}", $msg);
+        panic!("Fatal Error, cannot continue...");
+    }};
+}
+
+
+/// Dump the current state to the dump_file
+fn dump_state(state: StateStoreRef, dump_file: &mut File) -> Result<(), Box<dyn Error>> {
+    dump_file.seek(SeekFrom::Start(0))?;
+    rmps::encode::write(dump_file, &state)
+        .map_err(|e| fatal_panic!("Could not write state!", e.to_string()))
+        .unwrap();
+    Ok(())
+}
+
+/// Load state from the dump_file
+fn load_state() -> {
+
+}
--- src/startup.rs
+++ src/startup.rs
@@ -17,6 +17,13 @@ pub struct Config {
     /// Don't show the starting graphic
     #[structopt(short = "g", long = "no-graphic")]
     pub dont_show_graphic: bool,
+    #[structopt(short = "s", long = "ops-until-save", default_value = "10000")]
+    pub ops_until_save: u64,
+    #[structopt(short = "p", long = "port", default_value = "6379")]
+    pub port: u64,
+    /// Run in memory only mode. Don't save database state to disk
+    #[structopt(short = "m", long = "memory-only")]
+    pub memory_only: bool,
 }
 
 pub fn startup_message(config: &Config) {
