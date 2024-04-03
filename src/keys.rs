210172bed44d182ba0785ffbb1873fd8f9fecbe0
obaraelijah <elijahobara357@gmail.com>
2024-04-03 17:14:11 +0300

    feat: implimenting key value interactions

--- src/keys.rs
+++ src/keys.rs
@@ -1,9 +1,20 @@
 use crate::op_variants;
-use crate::types::{Key, Value};
+use crate::types::{Key, Value, ReturnValue, StateRef};
 
-// used to generate an enum with variants for different key operations.
 op_variants! {
     KeyOps,
     Set(Key, Value),
     Get(Key)
+}
+
+pub async fn key_interact(key_op: KeyOps, state: StateRef) -> ReturnValue {
+    match key_op {
+        KeyOps::Get(key) => state.kv.get(&key).map_or(ReturnValue::Nil, |v| {
+            ReturnValue::StringRes(v.value().clone())
+        }),
+        KeyOps::Set(key, value) => {
+            state.kv.insert(key, value);
+            ReturnValue::Ok
+        },
+    }   
 }
