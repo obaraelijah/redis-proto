7014984572f6ff19b13636699f38503f51f5c5da
obaraelijah <elijahobara357@gmail.com>
2024-04-03 15:10:22 +0300

    feat: Add compiler directives to enforce safety and improve code quality.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
+#![forbid(unsafe_code)]
+#![warn(clippy::all)]
+
 pub mod asyncresp;
 pub mod database;
 pub mod logger;
 pub mod startup;
 pub mod types;
 pub mod ops;
+#[macro_use]
 pub mod macros;
