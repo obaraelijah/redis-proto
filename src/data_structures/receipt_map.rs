b850c8e5d723b7d002bf990c4f79f7ec78b5d483
obaraelijah <elijahobara357@gmail.com>
2024-04-07 19:30:23 +0300

    feat: receipts  dsg

---
+++ src/data_structures/receipt_map.rs
@@ -0,0 +1,20 @@
+
+pub type Receipt = u32;
+
+pub enum KeyTypes {
+    List(u64),
+} 
+
+impl KeyTypes {
+    pub fn list() -> KeyTypes {
+        unimplemented!()
+    }
+}
+
+#[derive(Default, Debug)]
+pub struct ReceiptMap {
+    
+}
+impl ReceiptMap {
+    
+}
