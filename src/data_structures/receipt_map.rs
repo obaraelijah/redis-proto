afa2b90a2fc3c979846db0b628eba7721a56695f
obaraelijah <elijahobara357@gmail.com>
2024-04-08 09:07:20 +0300

    feat: receipt method implimentation

--- src/data_structures/receipt_map.rs
+++ src/data_structures/receipt_map.rs
@@ -1,20 +1,39 @@
+use std::{collections::{HashMap, HashSet}, task::Waker};
+use seahash::hash;
 
 pub type Receipt = u32;
 
+#[derive(Hash, Debug, PartialEq, Eq)]
 pub enum KeyTypes {
     List(u64),
 } 
 
 impl KeyTypes {
-    pub fn list() -> KeyTypes {
-        unimplemented!()
+    pub fn list(key: &[u8]) -> KeyTypes {
+        KeyTypes::List(hash(key))
     }
 }
 
 #[derive(Default, Debug)]
-pub struct ReceiptMap {
-    
+pub struct RecieptMap {
+    counter: Receipt,
+    wakers: HashMap<Receipt, Waker>,
+    timed_out: HashSet<Receipt>,
+    keys: HashMap<KeyTypes, Vec<Receipt>>,
 }
-impl ReceiptMap {
-    
+
+impl RecieptMap {
+    pub fn get_receipt(&mut self) -> Receipt {
+        self.counter += 1;
+        self.counter
+    }
+
+    pub fn insert(&mut self, receipt: Receipt, item: Waker, key: KeyTypes) {
+        self.wakers.insert(receipt, item);
+        self.keys.entry(key).or_default().push(receipt);
+    }
+
+    pub fn receipt_timed_out(&self, receipt: Receipt) -> bool {
+        self.timed_out.contains(&receipt)
+    }
 }
