8a632e2d635e35d42f800ca112c2dedeefae72cd
obaraelijah <elijahobara357@gmail.com>
2024-04-10 10:42:22 +0300

    feat: sorted sets methods

--- src/data_structures/sorted_set.rs
+++ src/data_structures/sorted_set.rs
@@ -26,6 +26,47 @@ impl Ord for SortedSetMember {
         self.partial_cmp(other).unwrap()
     }
 }
+// TODO: Look into using RangeBounds properly
+// impl RangeBounds<Score> for SortedSetMember {
+//     fn start_bound(&self) -> Bound<&Score> {
+//         Included(&self.score)
+//     }
+
+//     fn end_bound(&self) -> Bound<&Score> {
+//         Included(&self.score)
+//     }
+//     fn contains<U>(&self, item: &U) -> bool
+//     where
+//         U: PartialOrd<Score> + ?Sized,
+//     {
+//         if let Ordering::Equal = item.partial_cmp(&self.score).unwrap() {
+//             true
+//         } else {
+//             false
+//         }
+//     }
+// }
+
+// impl RangeBounds<SortedSetMember> for Range<Score> {
+//     fn start_bound(&self) -> Bound<&SortedSetMember> {
+//         let f = SortedSetMember::new(&b"".to_vec(), self.start);
+//         Included(f)
+//     }
+//     fn end_bound(&self) -> Bound<&SortedSetMember> {
+//         let f = SortedSetMember::new(&b"".to_vec(), self.end);
+//         Included(&f)
+//     }
+
+// fn contains<U>(&self, item: &U) -> bool
+// where
+//     U: PartialOrd<Score> + ?Sized {
+//     if let Ordering::Equal = item.partial_cmp(&self.score).unwrap() {
+//         true
+//     } else {
+//         false
+//     }
+// }
+// }
 
 impl SortedSetMember {
     fn new(key: &[u8], score: Score) -> Self {
@@ -75,5 +116,43 @@ impl SortedSet {
                 }
             })
             .sum()
+    }
+
+    /// Remove a single key from thr sorted set
+    fn remove_one(&mut self, key: &Key) {
+        self.members_hash.remove(key);
+    }
+
+    /// Returns the number of members stored in the set.
+    pub fn card(&self) -> Count {
+        self.members_hash.len() as Count
+    }
+
+    /// Return the score of the member in the sorted set
+    pub fn score(&self, key: Key) -> Option<Score> {
+        self.members_hash.get(&key).cloned()
+    }
+
+    /// Get all members between (lower, upper) scores
+    pub fn range(&self, range: (Score, Score)) -> RVec<SortedSetMember> {
+        // TODO: Use a more efficient method. I should use a skiplist or an AVL tree.
+        // Another option is to retackle the rangebounds stuff, but the semantics are different.
+        // I want to be able to compare by score AND member when inserting/removing,
+        // but only by score in this case. Need to figure out how to encode that.
+        self.scores
+            .iter()
+            .filter(|mem| range.0 <= mem.score && mem.score <= range.1)
+            .cloned()
+            .collect()
+    }
+
+    /// Remove count (default: 1) maximum members from the sorted set
+    pub fn pop_max(&mut self, count: Count) -> Vec<SortedSetMember> {
+        let count = count as usize; // TODO: What if it's negative?
+        let ret: Vec<SortedSetMember> = self.scores.iter().rev().take(count).cloned().collect();
+        for key in ret.iter().map(|s| s.member.clone()) {
+            self.remove(&[key.into()]);
+        }
+        ret
     }
 }
