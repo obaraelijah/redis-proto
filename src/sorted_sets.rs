7662c2eaaae2cf62487b03f879c7f46c844c9f75
obaraelijah <elijahobara357@gmail.com>
2024-04-12 14:18:48 +0300

    feat: Implement sortes set commands and appease clippy

--- src/sorted_sets.rs
+++ src/sorted_sets.rs
@@ -6,7 +6,11 @@ op_variants! {
     ZSetOps,
     ZAdd(Key, RVec<(Score, Key)>),
     ZRem(Key, RVec<Key>),
-    ZRange(Key, Score, Score)
+    ZRange(Key, Score, Score),
+    ZScore(Key, Key),
+    ZPopMax(Key, Count),
+    ZPopMin(Key, Count),
+    ZRank(Key, Key)
 }
 
 make_reader!(zsets, read_zsets);
@@ -40,5 +44,37 @@ pub async fn zset_interact(zset_op: ZSetOps, state: StateRef) -> ReturnValue {
             })
             .unwrap_or_default()
             .into(),
+        ZSetOps::ZScore(zset_key, member_key) => read_zsets!(state, &zset_key)
+            .and_then(|zset| zset.score(member_key))
+            .map(ReturnValue::IntRes)
+            .unwrap_or(ReturnValue::Nil),
+        ZSetOps::ZPopMax(zset_key, count) => write_zsets!(state, &zset_key)
+            .map(|mut zset| {
+                zset.pop_max(count)
+                    .into_iter()
+                    .fold(Vec::new(), |mut acc, zset_mem| {
+                        acc.push(ReturnValue::IntRes(zset_mem.score));
+                        acc.push(ReturnValue::StringRes(zset_mem.member.into()));
+                        acc
+                    })
+            })
+            .map(ReturnValue::Array)
+            .unwrap_or_else(|| ReturnValue::Array(vec![])),
+        ZSetOps::ZPopMin(zset_key, count) => write_zsets!(state, &zset_key)
+            .map(|mut zset| {
+                zset.pop_min(count)
+                    .into_iter()
+                    .fold(Vec::new(), |mut acc, zset_mem| {
+                        acc.push(ReturnValue::IntRes(zset_mem.score));
+                        acc.push(ReturnValue::StringRes(zset_mem.member.into()));
+                        acc
+                    })
+            })
+            .map(ReturnValue::Array)
+            .unwrap_or_else(|| ReturnValue::Array(vec![])),
+        ZSetOps::ZRank(zset_key, mem_key) => read_zsets!(state, &zset_key)
+            .and_then(|zset| zset.rank(mem_key))
+            .map(ReturnValue::IntRes)
+            .unwrap_or(ReturnValue::Nil),
     }
 }
