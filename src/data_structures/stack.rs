881ad3c0deb3e075ac28f6c0f0e2acd14ee49b76
obaraelijah <elijahobara357@gmail.com>
2024-04-12 15:26:47 +0300

    test: Stack methods and tests passed and appease clippy suggestions

--- src/data_structures/stack.rs
+++ src/data_structures/stack.rs
@@ -19,11 +19,38 @@ impl<T: Clone> Stack<T> {
         self.inner.pop()
     }
 
-    pub fn peek(&self) -> Option<T>{
+    pub fn peek(&self) -> Option<T> {
         self.inner.last().cloned()
     }
 
     pub fn size(&self) -> Count {
         self.inner.len() as Count
+    }
+}
+
+#[cfg(test)]
+mod test_stack {
+    use crate::data_structures::stack::Stack;
+
+    #[test]
+    fn test_push_pop() {
+        let mut s = Stack::new();
+        s.push(3);
+        assert_eq!(s.pop(), Some(3));
+        assert_eq!(s.pop(), None);
+    }
+
+    #[test]
+    fn test_peek_size() {
+        let mut s = Stack::new();
+        assert_eq!(s.size(), 0);
+        assert_eq!(s.peek(), None);
+        s.push(3);
+        assert_eq!(s.peek(), Some(3));
+        assert_eq!(s.peek(), Some(3));
+        assert_eq!(s.size(), 1);
+        s.push(4);
+        assert_eq!(s.peek(), Some(4));
+        assert_eq!(s.size(), 2);
     }
 }
