479705464910751e6de4c2c46aa3ec3f5d8d4259
obaraelijah <elijahobara357@gmail.com>
2024-04-16 15:25:19 +0300

    feat: Bench marking  the server

--- benches/keys_benchmark.rs
+++ benches/keys_benchmark.rs
@@ -1,6 +1,11 @@
-use bytes::{Bytes, BytesMut};
+use bytes::Bytes;
+use bytes::BytesMut;
 use criterion::{black_box, criterion_group, criterion_main, Criterion, Throughput};
 use redis_proto::asyncresp::RespParser;
+use redis_proto::keys::{key_interact, KeyOps};
+use redis_proto::ops::{op_interact, translate};
+use redis_proto::types::{RedisValueRef, ReturnValue, State, StateStore};
+use std::sync::Arc;
 use tokio_util::codec::Decoder;
 
 fn bench_parsing(c: &mut Criterion) {
@@ -20,13 +25,55 @@ fn bench_parsing(c: &mut Criterion) {
 }
 
 fn bench_translate(c: &mut Criterion) {
-    let values: Bytes = std::iter::repeat("a").take(200).collect::<String>().into();
-    
+    let value: Bytes = std::iter::repeat("a").take(200).collect::<String>().into();
+    let value = RedisValueRef::Array(vec![
+        RedisValueRef::SimpleString(Bytes::from_static(b"set")),
+        RedisValueRef::SimpleString(Bytes::from_static(b"foo")),
+        RedisValueRef::SimpleString(value),
+    ]);
+    let state_store = Arc::new(StateStore::default());
+    let mut group = c.benchmark_group("translate");
+    group.throughput(Throughput::Bytes(212));
+    group.bench_function("translate", |b| {
+        b.iter(|| translate(black_box(value.clone()), black_box(state_store.clone())));
+    });
+    group.finish();
+}
+
+fn bench_interact(c: &mut Criterion) {
+    let s = Arc::new(State::default());
+    c.bench_function("KeyOps::Set", |b| {
+        b.iter(|| async {
+            let f = KeyOps::Set(Bytes::from_static(b"foo"), Bytes::from_static(b"bar"));
+            key_interact(black_box(f), black_box(s.clone())).await;
+        });
+    });
+}
+
+fn bench_full_life_cycle(c: &mut Criterion) {
+    let state_store = Arc::new(StateStore::default());
+    c.bench_function("full_life_cycle", |b| {
+        b.iter(|| async {
+            let mut decoder = RespParser::default();
+            let s = Arc::new(State::default());
+            let scc = "*3\r\n$3\r\nset\r\n$3\r\nfoo\r\n$3\r\nbar\r\n";
+            let mut buf = BytesMut::from(format!("{}", scc).as_str());
+            let res = decoder
+                .decode(black_box(&mut buf))
+                .expect("parsing to work")
+                .unwrap();
+            let op = translate(black_box(res), black_box(state_store.clone())).unwrap();
+            let res = op_interact(black_box(op), black_box(s.clone())).await;
+            assert_eq!(res, ReturnValue::Ok);
+        });
+    });
 }
 
 criterion_group!(
     benches,
     bench_parsing,
+    bench_translate,
+    bench_interact,
+    bench_full_life_cycle
 );
-
 criterion_main!(benches);
